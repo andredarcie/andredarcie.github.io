7dc209222ccc3e693240aff539a4350d5c4b14a1
André N. Darcie <andrendarcie@gmail.com>
2026-06-13 08:30:39 -0300

    Remove tiny gta

--- tiny-gta/assets/models/missions/story-gem.js
+++
@@ -1,6 +0,0 @@
-import * as THREE from 'three';
-
-export function makeStoryGem(color){
-  return new THREE.Mesh(new THREE.OctahedronGeometry(.44,1),
-    new THREE.MeshBasicMaterial({color}));
-}
--- tiny-gta/assets/models/missions/story-marker.js
+++
@@ -1,7 +0,0 @@
-import * as THREE from 'three';
-
-export function makeStoryMarker(color){
-  const mat=new THREE.MeshBasicMaterial({color});
-  const marker=new THREE.Mesh(new THREE.OctahedronGeometry(.52,0),mat);
-  return{marker,mat};
-}
